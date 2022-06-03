fa145bd051d63dbc9724814e29df292baed913d9
Jopa-Przemyslaw <przemyslaw.jopa@gmail.com>
2022-06-03 16:51:40 +0200

    Added CommandService

---
+++ CommandService/Controllers/PlatformsController.cs
@@ -0,0 +1,23 @@
+﻿namespace CommandService.Controllers
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Route("api/c/[controller]")]
+    [ApiController]
+    public class PlatformsController : ControllerBase
+    {
+        public PlatformsController()
+        {
+                
+        }
+
+        [HttpPost]
+        public IActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Commands Service");
+
+            return Ok("Inbound test of Platforms Controller");
+        }
+    }
+}
